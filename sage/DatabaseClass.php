2693df07dec3dbe27086de4d53dd4666bf37ee95
vikramhegde <vikram.hegde@tallysolutions.com>
2021-03-26 14:32:26 +0530

    #1, singleton db design

--- sage/DatabaseClass.php
+++ sage/DatabaseClass.php
@@ -1,34 +1,65 @@
 <?php
-    class DatabaseClass  
-    {  
-        private $host = "localhost"; //  host name  
-        private $username = "root"; //  user name  
-        private $password = "root"; //  password  
-        private $db = "sage_student_project"; //  database name
-          
-        public function __construct()  
-        {  
-            $this-> conn = mysqli_connect($this -> host, $this -> username, $this -> password) or die(mysqli_connect_error("database"));  
-            mysqli_select_db($this-> conn, $this -> db) or die(mysqli_connect_error("database"));  
-        }  
+//database connection and queries in singleton pattern
+class DatabaseClass {
+	private $_connection;
+	private static $_instance; //singleton
+	private $_host = "localhost";
+	private $_username = "root";
+	private $_password = "root";
+	private $_database = "sage_student_project";
 
-        // this method used to execute mysql query         
-        public function execute_query($sql)  
-        {  
-            $c = mysqli_query($this-> conn, $sql) or die(mysqli_error("database")); 
-            return $c;  
-        } 
-   
-	// to get associative array 
-        public function get_fetched_data($r)  
-        {  
-		return $r->fetch_array(MYSQLI_ASSOC);
-        } 
+	// get instance of db
+	public static function getInstance() {
+		if(!self::$_instance) { // If no instance then make one
+			self::$_instance = new self();
+		}
+		return self::$_instance;
+	}
 
-	// to get row result	
+	// Constructor
+	private function __construct() {
+		try {
+		  $this->_connection = new PDO('mysql:host='.$this->_host.';dbname='.$this->_database, $this->_username, $this->_password);
+		  // set the PDO error mode to exception
+		  $this->_connection-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		} catch(PDOException $e) {
+		  echo "DB Connection failed: " . $e->getMessage();
+		}
+		
+	}
+
+	//prevent duplication of connection
+	private function __clone() { }
+
+	// Get PDO connection
+	public function getConnection() {
+		return $this->_connection;
+	}	
+
+	// execute query
+        public function execute_query( $statement = "" , $parameters = [] ){
+            try{
+			
+                $stmt = $this->_connection->prepare($statement);
+                $stmt->execute($parameters);
+                return $stmt;
+				
+            }catch(Exception $e){
+                throw new Exception($e->getMessage());   
+            }		
+        }
+
+	// fetch associative array query
+	public function get_fetched_data($r)  
+        {  
+		return $r->fetch(PDO::FETCH_ASSOC);
+        }
+
+	// fetch row query
 	public function get_row($r)  
         {  
-		return $r->fetch_row();
+		return $r->fetch();
         }  
-    }  
+
+}
 ?>
